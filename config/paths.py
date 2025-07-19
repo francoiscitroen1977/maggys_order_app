e063bdabff73f5d6f0bfc04e83a233c1ff39b888
francoiscitroen1977 <francois.citroen@islandrobotics.io>
2025-07-19 12:23:49 -0400

    Save matched and selected files to Newfiletemp

--- config/paths.py
+++ config/paths.py
@@ -5,5 +5,6 @@ BASE_DIR = Path(r"C:/Users/Public/ItemImport")
 
 FULL_PRICE_DIR = BASE_DIR / "Docs"
 NEW_ITEMS_DIR = BASE_DIR / "New"
+NEW_ITEMS_TEMP_DIR = BASE_DIR / "Newfiletemp"
 UPLOADED_PO_DIR = BASE_DIR / "UploadedPO"
 LOG_DIR = BASE_DIR / "log"
